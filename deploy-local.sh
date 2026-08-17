f80da9fa5241c88210d56f49bcec02041dc4bf31
Elthon Chocano <elthonchocano@gmail.com>
2026-08-16 21:29:57 -0700

    ft: deploy native

--- deploy-local.sh
+++ deploy-local.sh
@@ -5,6 +5,6 @@ echo "Step 1: Compiling native binary..."
 ./mvnw clean package -Dnative
 
 echo "Step 2: Compressing native binary with UPX..."
-upx --best target/tennis-backend-1.0.0-SNAPSHOT-runner
+upx-ucl --best target/tennis-backend-1.0.0-SNAPSHOT-runner
 
 echo "Native compilation and compression complete. Run 'git commit' and 'git push' to deploy."
